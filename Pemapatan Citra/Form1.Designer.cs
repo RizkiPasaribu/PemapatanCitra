a2b4d2c3b1a3ce425090b576bf4837c22c44878c
Rizki Pasaribu <innet71050500@gmail.com>
2022-07-06 09:11:26 +0700

    first commit

---
+++ Pemapatan Citra/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Pemapatan_Citra
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Browse = new System.Windows.Forms.Button();
+            this.button_Compress = new System.Windows.Forms.Button();
+            this.pictureBox_Image = new System.Windows.Forms.PictureBox();
+            this.textBox_Path = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Image)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_Browse
+            // 
+            this.button_Browse.Location = new System.Drawing.Point(12, 8);
+            this.button_Browse.Name = "button_Browse";
+            this.button_Browse.Size = new System.Drawing.Size(140, 23);
+            this.button_Browse.TabIndex = 0;
+            this.button_Browse.Text = "Browse";
+            this.button_Browse.UseVisualStyleBackColor = true;
+            this.button_Browse.Click += new System.EventHandler(this.button_Browse_Click);
+            // 
+            // button_Compress
+            // 
+            this.button_Compress.Location = new System.Drawing.Point(194, 300);
+            this.button_Compress.Name = "button_Compress";
+            this.button_Compress.Size = new System.Drawing.Size(147, 23);
+            this.button_Compress.TabIndex = 1;
+            this.button_Compress.Text = "Compress";
+            this.button_Compress.UseVisualStyleBackColor = true;
+            this.button_Compress.Click += new System.EventHandler(this.button_Compress_Click);
+            // 
+            // pictureBox_Image
+            // 
+            this.pictureBox_Image.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox_Image.Location = new System.Drawing.Point(12, 63);
+            this.pictureBox_Image.Name = "pictureBox_Image";
+            this.pictureBox_Image.Size = new System.Drawing.Size(329, 231);
+            this.pictureBox_Image.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_Image.TabIndex = 2;
+            this.pictureBox_Image.TabStop = false;
+            // 
+            // textBox_Path
+            // 
+            this.textBox_Path.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox_Path.Location = new System.Drawing.Point(12, 37);
+            this.textBox_Path.Name = "textBox_Path";
+            this.textBox_Path.ReadOnly = true;
+            this.textBox_Path.Size = new System.Drawing.Size(329, 20);
+            this.textBox_Path.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(355, 331);
+            this.Controls.Add(this.textBox_Path);
+            this.Controls.Add(this.pictureBox_Image);
+            this.Controls.Add(this.button_Compress);
+            this.Controls.Add(this.button_Browse);
+            this.Name = "Form1";
+            this.Text = "Rizki Ali Akbar Pasaribu (0701192007)";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Image)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Browse;
+        private System.Windows.Forms.Button button_Compress;
+        private System.Windows.Forms.PictureBox pictureBox_Image;
+        private System.Windows.Forms.TextBox textBox_Path;
+    }
+}
+
---
+++ Pemapatan Citra/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace Pemapatan_Citra
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Browse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Title = "Browse Image";
+            openFile.Filter = "Choose Image (*.jpg) |*jpg";
+
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                textBox_Path.Text = openFile.FileName;
+                Image image = Image.FromFile(textBox_Path.Text);
+                pictureBox_Image.Image = image;
+            }
+        }
+
+        private void button_Compress_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                Image gambar = Image.FromFile(textBox_Path.Text);
+                int kualitasGambar = 30;
+                string simpanFile = textBox_Path.Text.Insert(textBox_Path.Text.Length - 4, " Compressed");
+
+                //Membuat ImageCodecInfo object untuk informasi pematapan gambar
+                ImageCodecInfo jpegCodec = null;
+
+                //Mangatur kualitas gambar yang akan di hasilkan
+                EncoderParameter imageQualitysParameter = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, kualitasGambar);
+
+                //Mendapat kan algoritma encoding/compress/pemapatan
+                ImageCodecInfo[] allCodecs = ImageCodecInfo.GetImageEncoders();
+
+                EncoderParameters codecParameter = new EncoderParameters(1);
+                codecParameter.Param[0] = imageQualitysParameter;
+
+                // Mencari algorima yang cocok dengan gambar. yang sebelum nya
+                // telah kita dapat dari object Image Code Info diatas
+                for (int i = 0; i < allCodecs.Length; i++)
+                {
+                    if (allCodecs[i].MimeType == "image/jpeg")
+                    {
+                        jpegCodec = allCodecs[i];
+                        break;
+                    }
+                }
+
+                //Simpan Gambar
+                gambar.Save(simpanFile, jpegCodec, codecParameter);
+                MessageBox.Show("Gambar Berhasil Dimapatkan","Berhasil");
+                MessageBox.Show(simpanFile, "Gambar Di Simpan");
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+    }
+}
